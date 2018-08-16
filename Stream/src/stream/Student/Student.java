ad82e8cdf241f75f5cb3bb8e139804c6cf7edd18
slicing <sunjiaoyong@xiyouant.org>
2018-08-16 19:08:26 +0800

    Stream commit

---
+++ Stream/src/stream/Student/Student.java
@@ -0,0 +1,94 @@
+package stream.Student;
+
+import java.time.LocalDate;
+
+public class Student {
+    public enum Gender{
+        FEMALE,MALE
+    }
+    public enum Department{
+        CS,CN,EE,SM,CL,EL
+    }
+    private Long id;
+    private String name;
+    private Gender gender;
+    private Integer credit;
+    private LocalDate entryDate;
+    private Department department;
+
+    public Student(Long id, String name, Gender gender, Integer credit, LocalDate entryDate, Department department) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.credit = credit;
+        this.entryDate = entryDate;
+        this.department = department;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public Integer getCredit() {
+        return credit;
+    }
+
+    public void setCredit(Integer credit) {
+        this.credit = credit;
+    }
+
+    public LocalDate getEntryDate() {
+        return entryDate;
+    }
+
+    public void setEntryDate(LocalDate entryDate) {
+        this.entryDate = entryDate;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public boolean isMale(){
+        return this.gender == Gender.MALE;
+    }
+    public boolean isFaMale(){
+        return this.gender == Gender.FEMALE;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", gender=" + gender +
+                ", credit=" + credit +
+                ", entryDate=" + entryDate +
+                ", department=" + department +
+                '}';
+    }
+}
