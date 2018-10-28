789879c308a13bce4a189609d873c52a71f2e9ca
slicing <slow_slicing@qq.com>
2018-10-28 22:18:47 +0800

    Navigation page switching with Bootstrap And AngularJS

--- spring-boot-Bootstrap-demo/src/main/java/com/example/demo/DemoApplication.java
+++ spring-boot-Bootstrap-demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,10 +2,19 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.awt.*;
+
+@RestController
 @SpringBootApplication
 public class DemoApplication {
-
+	@RequestMapping(value = "/search",produces = {MediaType.APPLICATION_JSON_VALUE})
+	public Person search(String personName){
+		return new Person(personName,32,"hefei");
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
